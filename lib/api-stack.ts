d3f3fa501d8d12141b87cf3106ad0bc4edb96615
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-08-23 20:07:41 +0530

    disable codedeploy canary

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -2,10 +2,10 @@ import * as cdk from '@aws-cdk/core'
 import * as apiGw from '@aws-cdk/aws-apigateway'
 // import * as cd from '@aws-cdk/aws-codedeploy'
 // import * as cw from '@aws-cdk/aws-cloudwatch'
-import * as lambda from '@aws-cdk/aws-lambda'
+// import * as lambda from '@aws-cdk/aws-lambda'
 import { Lambda } from './helpers'
 
-const aliasName = 'stage'
+// const aliasName = 'stage'
 
 export class ApiStack extends cdk.Stack {
   readonly apiURL: cdk.CfnOutput
@@ -15,14 +15,14 @@ export class ApiStack extends cdk.Stack {
 
     // Function and its alias
     const handler = new Lambda(this, 'apiHandler')
-    const stage = new lambda.Alias(this, 'apiHandlerDev', {
-      aliasName,
-      version: handler.currentVersion,
-    })
+    // const stage = new lambda.Alias(this, 'apiHandlerDev', {
+    //   aliasName,
+    //   version: handler.currentVersion,
+    // })
 
     // API endpoint
     const api = new apiGw.LambdaRestApi(this, 'restApi', {
-      handler: stage,
+      handler,
       deployOptions: { stageName: 'dev' },
     })
 
