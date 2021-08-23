de2a44c8953becc7107a570ab2f35e125cb59459
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-08-23 19:59:48 +0530

    disable codedeploy canary

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core'
 import * as apiGw from '@aws-cdk/aws-apigateway'
-import * as cd from '@aws-cdk/aws-codedeploy'
-import * as cw from '@aws-cdk/aws-cloudwatch'
+// import * as cd from '@aws-cdk/aws-codedeploy'
+// import * as cw from '@aws-cdk/aws-cloudwatch'
 import * as lambda from '@aws-cdk/aws-lambda'
 import { Lambda } from './helpers'
+
+const aliasName = 'stage'
 
 export class ApiStack extends cdk.Stack {
   readonly apiURL: cdk.CfnOutput
@@ -14,7 +16,7 @@ export class ApiStack extends cdk.Stack {
     // Function and its alias
     const handler = new Lambda(this, 'apiHandler')
     const stage = new lambda.Alias(this, 'apiHandlerDev', {
-      aliasName: 'stage',
+      aliasName,
       version: handler.currentVersion,
     })
 
@@ -24,27 +26,27 @@ export class ApiStack extends cdk.Stack {
       deployOptions: { stageName: 'dev' },
     })
 
-    const failureAlarm = new cw.Alarm(this, 'lambdaFailure', {
-      alarmDescription: 'The latest deployment errors > 0',
-      metric: new cw.Metric({
-        metricName: 'Errors',
-        namespace: 'AWS/Lambda',
-        statistic: 'sum',
-        // dimensionsMap: {
-        //   Resource: `${handler.functionArn}:stage`,
-        //   FunctionName: handler.functionArn,
-        // },
-        period: cdk.Duration.minutes(1),
-      }),
-      threshold: 1,
-      evaluationPeriods: 1,
-    })
+    // const failureAlarm = new cw.Alarm(this, 'lambdaFailure', {
+    //   alarmDescription: 'The latest deployment errors > 0',
+    //   metric: new cw.Metric({
+    //     metricName: 'Errors',
+    //     namespace: 'AWS/Lambda',
+    //     statistic: 'sum',
+    //     // dimensionsMap: {
+    //     //   Resource: `${handler.functionArn}:${aliasName}`,
+    //     //   FunctionName: handler.functionArn,
+    //     // },
+    //     period: cdk.Duration.minutes(1),
+    //   }),
+    //   threshold: 1,
+    //   evaluationPeriods: 1,
+    // })
 
-    new cd.LambdaDeploymentGroup(this, 'canaryDeployment', {
-      alias: stage,
-      deploymentConfig: cd.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
-      alarms: [failureAlarm],
-    })
+    // new cd.LambdaDeploymentGroup(this, 'canaryDeployment', {
+    //   alias: stage,
+    //   deploymentConfig: cd.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+    //   alarms: [failureAlarm],
+    // })
 
     this.apiURL = new cdk.CfnOutput(this, 'apiURL', {
       value: api.url,
