af282a5552b8cfb18678a1a9ae9b3fa32f39bf74
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-08-25 18:55:43 +0530

    fix error

--- functions/apiHandler.ts
+++ functions/apiHandler.ts
@@ -1,11 +1,9 @@
 import { ProxyHandler } from 'aws-lambda'
 
 export const handler: ProxyHandler = async (event) => {
-  if (Math.random() > 0.5) throw Error('an unexpected error occured!')
-
   return {
     body: JSON.stringify({
-      message: 'API version 2 has been deployed!',
+      message: 'API version 3 has been deployed!',
       path: event.path,
     }),
     headers: { 'Content-Type': 'application/json' },
