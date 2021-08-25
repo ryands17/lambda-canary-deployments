75212eeb20a77adf586d13b3cf4828b2664b447c
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-08-25 17:52:50 +0530

    update API version to 2

--- functions/apiHandler.ts
+++ functions/apiHandler.ts
@@ -3,7 +3,7 @@ import { ProxyHandler } from 'aws-lambda'
 export const handler: ProxyHandler = async (event) => {
   return {
     body: JSON.stringify({
-      message: 'API version 1 has been deployed!',
+      message: 'API version 2 has been deployed!',
       path: event.path,
     }),
     headers: { 'Content-Type': 'application/json' },
